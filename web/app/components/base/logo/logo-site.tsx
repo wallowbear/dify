4002505933f96a0f28f3fcb0f685e0dfebab9849
zhangzhihong <culapicar@163.com>
2025-02-26 13:52:26 +0800

    Update Dockerfile and logo configuration
    - Modify base Docker image to use standard Alpine version
    - Update API and marketplace URLs in production environment
    - Replace default logo with custom logo file

--- web/app/components/base/logo/logo-site.tsx
+++ web/app/components/base/logo/logo-site.tsx
@@ -11,7 +11,7 @@ const LogoSite: FC<LogoSiteProps> = ({
 }) => {
   return (
     <img
-      src={'/logo/logo.png'}
+      src={'/logo/logo-bwhr.png'}
       className={classNames('block w-[22.651px] h-[24.5px]', className)}
       alt='logo'
     />
